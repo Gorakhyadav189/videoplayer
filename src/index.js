672af9435f396efc4704f7b405a37ed1c4e606bd
Gorakhyadav189 <gorakhyadav189@gmail.com>
2025-04-29 02:11:05 +0530

    third

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ dotenv.config({
 
 
 connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("app error:", error);
+            throw error
+
+        })
+        app.listen(process.env.PORT || 8000, () => {
+
+            console.log(`server is running at port :${process.env.PORT}`)
+        })
+
+    })
+    .catch((error) => {
+        console.log("db connection faild", error);
+    })
 
 
 
---
+++ src/utils/asynchandeler.js
@@ -0,0 +1,27 @@
+// const asyncHandler = () => { }
+
+export { asyncHandler }
+
+
+
+// const asyncHandler = () => {
+
+// }
+// const asyncHandler = (func) => {
+
+// }
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+
+
+    }
+
+
+
+}
